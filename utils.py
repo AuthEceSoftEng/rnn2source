6596d9817da923d2317eea15b8e572f632b2c9cb
vasilis <mvasilis@auth.gr>
2016-11-01 17:19:05 +0200

    Add linguist binary/auto-generated checking

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from linguist.libs.file_blob import FileBlob
+
 
 def sample(preds, temperature=0.35):
     # helper function to sample an index from a probability array
@@ -21,17 +23,51 @@ def jsparser(path):
     t1 = time.time()
     for root, dirs, files in os.walk(path):
         for name in files:
-            if name.endswith(".js"):
-                with open(os.path.join(root, name)) as js_file:
-                    data = js_file.read()
-                if len(data) > 11:
-                    minidata = jsmin(data) + '\xfe'
-                    minified_data.append(minidata)
-
+            if FileBlob(os.path.join(root, name)).is_generated:
+                if name.endswith(".js"):
+                    with open(os.path.join(root, name)) as js_file:
+                        data = js_file.read()
+                        print data
+                    if len(data) > 11:
+                        minidata = jsmin(data) + '\xfe'
+                        minified_data.append(minidata)
+                        minified_data = '\xff'.join(minified_data)
     minified_data = '\xff'.join(minified_data)
 
     t2 = time.time()
     print "Created the dataset in: %f milliseconds" % ((t2 - t1) * 1000.)
+
+    return minified_data
+
+
+def jsparserling(path):
+    from linguist.libs.file_blob import FileBlob
+    import os
+    import time
+    from jsmin import jsmin
+
+    path = '/home/vasilis/Documents/projects'
+    print "Reading data..."
+    minified_data = ['']
+    t1 = time.time()
+    filecounter = 0
+    excluded = set(['test', 'tests', '__tests__' 'locale', 'locales', 'ngLocale'])
+    for root, dirs, files in os.walk(path, topdown=True):
+        dirs[:] = [d for d in dirs if d not in excluded]  # exclude test directories
+        for name in files:
+            if name.endswith(".js"):
+                blob = FileBlob(os.path.join(root, name))
+                if not (blob.is_binary or blob.is_generated):
+                    filecounter += 1
+                    print os.path.join(root, name)
+                    with open(os.path.join(root, name)) as js_file:
+                        data = js_file.read()
+                        minidata = jsmin(data) + '\xfe'
+                        minified_data.append(minidata)
+    minified_data = '\xff'.join(minified_data)
+
+    t2 = time.time()
+    print "Created the dataset in: %f milliseconds from %d files" % ((t2 - t1) * 1000., filecounter)
 
     return minified_data
 
