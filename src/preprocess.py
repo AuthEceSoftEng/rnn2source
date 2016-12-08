dbf48d6deae87f2f5f6b0177407100a9dde66a70
bvasilis <mvasilis@auth.gr>
2016-12-08 21:34:38 +0200

    Complete Unicode approach

--- src/preprocess.py
+++ src/preprocess.py
@@ -11,9 +11,9 @@ from pygments.token import Token
 # path = '/home/vasilis/Documents/projects'   # TODO: Use argparse to get that
 path = '/home/vasilis/Documents/projects/mbostock-d3-b516d77/src/geo'
 print "Reading data..."
+t1 = time.time()
 minified_data = []
 label_data = []
-t1 = time.time()
 filecounter = 0
 excluded = {'test', 'tests', '__tests__' 'locale', 'locales', 'ngLocale'}
 point = JavascriptLexer()
@@ -26,40 +26,38 @@ for root, dirs, files in os.walk(path, topdown=True):
             if not (blob.is_binary or blob.is_generated):
                 with open(os.path.join(root, name)) as js_file:
                     minidata = jsmin(js_file.read())
-                    if minidata not in minified_data:
-                        filecounter += 1
-                        labels = ''
-                        for token in point.get_tokens_unprocessed(minidata):
-                            (_, type, seq) = token
-                            if type is Token.Literal.String.Regex:
-                                tag = 'r'  # regex
-                            elif type is Token.Keyword:
-                                tag = 'k'  # keyword
-                            elif type is Token.Literal.String:
-                                tag = 's'  # string
-                            elif type is Token.Literal.Number:
-                                tag = 'n'  # number
-                            elif type is Token.Operator:
-                                tag = 'o'  # operator
-                            elif (type is Token.Text) or (type is Token.Punctuation):
-                                tag = 'p'  # punctuation
-                            elif type is Token.Name:
-                                tag = 'i'  # identifier
-                            else:
-                                tag = 'e'  # others
+                    # if minidata not in minified_data:
+                filecounter += 1
+                labels = ''
+                chars = ''
+                for type, seq in point.get_tokens(minidata):
+                    chars += seq
+                    if type in Token.Literal.String.Regex:
+                        tag = 'r'  # regex
+                    elif type in Token.Keyword:
+                        tag = 'k'  # keyword
+                    elif type in Token.Literal.String:
+                        tag = 's'  # string
+                    elif type in Token.Literal.Number:
+                        tag = 'n'  # number
+                    elif type in Token.Operator:
+                        tag = 'o'  # operator
+                    elif (type in Token.Text) or (type in Token.Punctuation):
+                        tag = 'p'  # punctuation
+                    elif type is Token.Name:
+                        tag = 'i'  # identifier
+                    else:
+                        tag = 'e'  # others
 
-                            for i in range(len(seq)):
-                                # if not len(labels) == len(minidata):    # other, not for,  solution
-                                    labels += tag   # join instead
-                        if len(minidata) == len(labels):
-                            print 'something is going wrong'
-                            print len(minidata), len(labels)
-                            print minidata
-                            print labels
+                    for i in range(len(seq)):
+                        # if not len(labels) == len(minidata):    # other, not for,  solution
+                        labels += tag   # join instead
 
-                        if len(labels) == len(minidata):
-                            minified_data.append(minidata)
-                            label_data.append(labels)
+
+
+                if len(labels) == len(chars):
+                    minified_data.append(chars)
+                    label_data.append(labels)
 
 
 with open('chars', 'wb') as f:
@@ -69,11 +67,13 @@ with open('labels', 'wb') as f:
     pickle.dump(label_data, f)
 
 minified_data = ''.join(minified_data)
+label_data = ''.join(label_data)
 
 t2 = time.time()
-print "Created the dataset in: %f milliseconds from %d files" % ((t2 - t1) * 1000., filecounter)
-
-chars = list(set(minified_data))
+chars = sorted(list(set(minified_data)))
 data_size, vocab_size = len(minified_data), len(chars)
-print 'data has %d characters, %d unique.' % (data_size, vocab_size)
-print chars
+print "Created the dataset in: %f milliseconds from %d files." % ((t2 - t1) * 1000., filecounter)
+print 'Data has %d characters, %d unique.' % (data_size, vocab_size)
+print len(label_data)
+# for c, l in zip(minified_data, label_data):
+#     print c, l
