68d131b267d1660ed3cfe495360ee33b1d577033
bvasilis <mvasilis@auth.gr>
2016-12-08 19:09:48 +0200

    Test byte representation

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,13 +1,15 @@
 import os
 import pickle
 import time
-from pygments.lexers.javascript import JavascriptLexer
-from pygments.token import Token
 
 from jsmin import jsmin
 from linguist.libs.file_blob import FileBlob
+from pygments.lexers.javascript import JavascriptLexer
+from pygments.token import Token
 
-path = '/home/vasilis/Documents/projects'
+
+# path = '/home/vasilis/Documents/projects'   # TODO: Use argparse to get that
+path = '/home/vasilis/Documents/projects/mbostock-d3-b516d77/src/geo'
 print "Reading data..."
 minified_data = []
 label_data = []
@@ -16,7 +18,6 @@ filecounter = 0
 excluded = {'test', 'tests', '__tests__' 'locale', 'locales', 'ngLocale'}
 point = JavascriptLexer()
 
-print type(label_data)
 for root, dirs, files in os.walk(path, topdown=True):
     dirs[:] = [d for d in dirs if d not in excluded]  # exclude test directories
     for name in files:
@@ -24,33 +25,38 @@ for root, dirs, files in os.walk(path, topdown=True):
             blob = FileBlob(os.path.join(root, name))  # Linguist file checking
             if not (blob.is_binary or blob.is_generated):
                 with open(os.path.join(root, name)) as js_file:
-                    data = js_file.read()
-                    minidata = '\xff' + jsmin(data) + '\xfe'
+                    minidata = jsmin(js_file.read())
                     if minidata not in minified_data:
                         filecounter += 1
                         labels = ''
-                        for token in point.get_tokens(minidata):
-                            (type, seq) = token
-                            if type == Token.Literal.String.Regex:
+                        for token in point.get_tokens_unprocessed(minidata):
+                            (_, type, seq) = token
+                            if type is Token.Literal.String.Regex:
                                 tag = 'r'  # regex
-                            elif type in Token.Keyword:
+                            elif type is Token.Keyword:
                                 tag = 'k'  # keyword
-                            elif type in Token.Literal.String:
+                            elif type is Token.Literal.String:
                                 tag = 's'  # string
-                            elif type in Token.Literal.Number:
+                            elif type is Token.Literal.Number:
                                 tag = 'n'  # number
-                            elif type in Token.Operator:
+                            elif type is Token.Operator:
                                 tag = 'o'  # operator
-                            elif (type in Token.Text) or (type in Token.Punctuation):
+                            elif (type is Token.Text) or (type is Token.Punctuation):
                                 tag = 'p'  # punctuation
-                            elif type in Token.Name:
+                            elif type is Token.Name:
                                 tag = 'i'  # identifier
                             else:
                                 tag = 'e'  # others
 
                             for i in range(len(seq)):
-                                if not len(labels) == len(minidata):
-                                    labels += tag
+                                # if not len(labels) == len(minidata):    # other, not for,  solution
+                                    labels += tag   # join instead
+                        if len(minidata) == len(labels):
+                            print 'something is going wrong'
+                            print len(minidata), len(labels)
+                            print minidata
+                            print labels
+
                         if len(labels) == len(minidata):
                             minified_data.append(minidata)
                             label_data.append(labels)
@@ -70,3 +76,4 @@ print "Created the dataset in: %f milliseconds from %d files" % ((t2 - t1) * 100
 chars = list(set(minified_data))
 data_size, vocab_size = len(minified_data), len(chars)
 print 'data has %d characters, %d unique.' % (data_size, vocab_size)
+print chars
